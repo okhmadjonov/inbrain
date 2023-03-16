20047080f5826b18ca13888a195961324060ef0d
unknown <oakhmadjonov1@gmail.com>
2023-03-16 20:45:10 +0500

    App page routes added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Home from "../../pages/Home/Home";
 import "./App.scss";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" replace={true} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
