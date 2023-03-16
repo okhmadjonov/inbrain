cd7cdf420ad173c7f0362d200acfb5351bf32f49
unknown <oakhmadjonov1@gmail.com>
2023-03-16 20:06:53 +0500

    Store and reducer files added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
 import App from "./components/App/App";
-
+import { Provider } from "react-redux";
+import store from "./reduxToolkit/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
