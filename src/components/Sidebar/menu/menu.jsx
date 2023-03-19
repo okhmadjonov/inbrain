70dceea339930b314e715830a5e9aedc0920fdc7
BehzodJalolov <111660879+Bekhzod02@users.noreply.github.com>
2023-03-19 08:19:02 +0300

    sidebar

--- src/components/Sidebar/menu/menu.jsx
+++ src/components/Sidebar/menu/menu.jsx
@@ -14,6 +14,7 @@ import SubMenu from "./SubMenu";
 import { IconContext } from "react-icons/lib";
 
 import "./menu.scss";
+import { fontSize } from "@mui/system";
 
 const DropdownLink = styled(Link)`
   display: flex;
@@ -97,21 +98,21 @@ const Sidebar = () => {
               <AiIcons.AiOutlineClose style={{color: '#ffcc00'}} onClick={showSidebar} />
             </Navopen>
 
-            <Accordion style={{marginTop: '80px', background: 'none', color: '#FFFFFF'  }} >
+            <Accordion style={{marginTop: '40px', background: 'none', color: '#FFFFFF', marginLeft: '7%'   }} >
                 <AccordionSummary
-                  expandIcon={<MdExpandMore/>}
+                  expandIcon={<MdExpandMore style={{fontSize: '2.5rem'}} />}
                   aria-controls="panel1a-content"
                   id="panel1a-header"
                 >
                   <Typography>
                       <div style={{display: 'flex', textAlign: 'center', alignItems: 'center'}} >
                         <div className="avatar">
-                          <img style={{borderRadius: '30px'}} src={petrov} />
+                          <img style={{borderRadius: '30px', width: '40px', height: '40px'}} src={petrov} />
                             </div>
 
                               <div className="ml-3">
                                 <p className="m-0 p-0">UserName</p>
-                                  <span style={{fontSize: '14px', color: '#ffcc00'}} >balance: 99 000 сум</span>
+                                  <span style={{fontSize: '11px', color: '#ffcc00'}} >balance: 99 000 сум</span>
                               </div>
                       </div>
                   </Typography>
@@ -123,17 +124,354 @@ const Sidebar = () => {
                     </Link>
                   </Typography>
                 </AccordionDetails>
-              </Accordion>
+            </Accordion>
+            <Accordion style={{marginTop: '10px', background: 'none', color: '#FFFFFF', marginLeft: '7%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2.5rem', color: '#1C84FF'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#1C84FF', fontSize: '20px', fontFamily: 'Inter'}} >
+                        Веб разработчик
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link style={{color: '#FFFFFF'}} >
+                        Lorem
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%' }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', fontSize: '18px', fontFamily: 'Inter'}} >
+                        Приветствие
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography style={{display: 'grid', gap: '20px'}}>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}} >
+                      <Link style={{color: '#FFFFFF'}}>
+                        Привет
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}}>
+                        Что такое веб-разработка?
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}}>
+                        Про трудоустройство
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Как устроено обучение
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Программа обучения
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Какие проекты <br/> предстоит делать?
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Про реальные проекты
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Сколько времени придётся <br/> тратить на учёбу?
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Будет сложно. <br/> Но у Вас получится                      
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Начать обучение
+                      </Link>
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      <Link style={{color: '#FFFFFF'}} >
+                        Что такое веб-разработка?                    
+                      </Link>
+                    </li>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center'}} >
+                        HTML 
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography style={{display: 'grid', gap: '20px'}}>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}} >
+                      Базовые элементы HTML
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Теги HTML
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Заголовки
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Абзац
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 4
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 5
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 6
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 7
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 8
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 9
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 10
+                    </li>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}}>
+                      Урок 11
+                    </li>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px', color: '#5E5858'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#5E5858'}} >
+                        CSS
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link style={{color: '#FFFFFF'}} >
+                      Lorem
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px', color: '#5E5858'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#5E5858' }} >
+                          Javascript
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link to={'/Userpage'} style={{color: '#FFFFFF'}} >
+                      Userpage
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '10px', background: 'none', color: '#FFFFFF', marginLeft: '7%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2.5rem', color: '#1C84FF',  }} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', fontSize: '20px', color: '#1C84FF', fontFamily: 'Inter'}} >
+                        Справочник
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link to={'/Userpage'} style={{color: '#FFFFFF'}} >
+                      Userpage
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center'}} >
+                        HTML 
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography style={{display: 'grid', gap: '20px'}}>
+                    <li style={{color: '#FFFFFF', fontSize: '12px'}} >
+                      lorem
+                    </li>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px', color: '#5E5858'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#5E5858'}} >
+                        CSS
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link style={{color: '#FFFFFF'}} >
+                      Lorem
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px', color: '#5E5858'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#5E5858' }} >
+                          Javascript
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link to={'/Userpage'} style={{color: '#FFFFFF'}} >
+                      Userpage
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '10px', background: 'none', color: '#FFFFFF', marginLeft: '7%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2.5rem', color: '#1C84FF',  }} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', fontSize: '20px', color: '#1C84FF', fontFamily: 'Inter'}} >
+                        Экспресс тесты
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link to={'/Userpage'} style={{color: '#FFFFFF'}} >
+                      Userpage
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>   
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center'}} >
+                        HTML 
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link style={{color: '#FFFFFF'}} >
+                      Lorem
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px', color: '#5E5858'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#5E5858'}} >
+                        CSS
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link style={{color: '#FFFFFF'}} >
+                      Lorem
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion style={{marginTop: '5px', background: 'none', color: '#FFFFFF', marginLeft: '10%'  }} >
+                <AccordionSummary
+                  expandIcon={<MdExpandMore style={{fontSize: '2rem', marginRight: '5px', color: '#5E5858'}} />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>
+                      <div style={{display: 'flex', textAlign: 'center', alignItems: 'center', color: '#5E5858' }} >
+                          Javascript
+                      </div>
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <Link to={'/Userpage'} style={{color: '#FFFFFF'}} >
+                      Userpage
+                    </Link>
+                  </Typography>
+                </AccordionDetails>
+            </Accordion>
             
-            {SidebarData.map((item, index) => {
-              return (
-                <>
-                  <div>
-                    <SubMenu item={item} key={index} />
-                  </div>
-                </>
-              );
-            })}
+            
           </SidebarWrap>
         </SidebarNav>
       </IconContext.Provider>
