7f09c32e3b650d3ffcc948c31192b26863bcae08
AlmazKhambarov <almazkhambarov07@gmail.com>
2023-03-28 15:55:36 +0500

    modal end

--- src/pages/Handbook/Html/tags/modal/Modal.jsx
+++ src/pages/Handbook/Html/tags/modal/Modal.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import close_icon from "../../../../../assets/images/close_icon.png";
 import "./Modal.scss";
 
 export default function Modal({ modal, titles, toggleModal }) {
   // const [modal, setModal] = useState(false);
+  const {t} = useTranslation()
  const item = '<!--...--!>'
   if (modal) {
     document.body.classList.add("active-modal");
@@ -24,12 +26,7 @@ export default function Modal({ modal, titles, toggleModal }) {
             <br/>
             <br/>
             <p>
-              Страницы сайтов собирают из элементов, как конструктор LEGO.
-              Текстовые блоки, картинки, ссылки, кнопки — всё это элементы
-              сайта. Элементы описывают на языке HTML. Любая веб-страница
-              содержит код на этом языке и сохраняется в HTML-файле. Когда вы
-              открываете сайт, именно файл с HTML-кодом первым делом загружается
-              в браузер.
+              {t('modal.1')}
             </p>
             <img
               onClick={toggleModal}
